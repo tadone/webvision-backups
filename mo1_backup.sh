ee5178ec9b9bdfb253e1a522d3694a77c352800a
Tadeusz Swider <tadeusz.swider@motorolasolutions.com>
2017-09-27 16:56:44 +0200

    Fix order, add absolute path for mysqldump

--- mo1_backup.sh
+++ mo1_backup.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
 # Backup script for magento servers
 # 2017-09-27 by Tad Swider
+# v2
 
-set -e # Exit if a command fails
-
-# Check if tools exist
-if [ ! -x "$(command -v aws)" ]; then
-   log_message "ERROR - AWS CLI not installed. Exiting..."
-   exit 1
-fi
-
-if [ ! -x "$(command -v aws)" ]; then
-   log_message "ERROR - MySQLDump not found. Exiting..."
-   exit 1
-fi
+#set -e # Exit if a command fails
 
 # Set the date format, filename and the directories where your backup files
 # will be placed and which directory will be archived.
-NOW=$(date +"%Y-%m-%d")
+NOW=$(date +'%Y-%m-%d')
 SITENAME="magento1-52-73-17-216"
 FILE="$SITENAME-$NOW.tar"
 BACKUP_DIR="/home/bitnami/backups"
@@ -25,58 +15,64 @@ WWW_DIR="/home/bitnami/apps/magento/htdocs"
 LOG_FILE="/home/bitnami/backups/backup.log"
 S3_BUCKET='s3://webvision-backups'
 
+echo "Starting..." >> "$LOG_FILE"
 # MySQL database credentials
-
+DB_USER="CHANGE_ME"
+DB_PASS="CHANGE_ME"
+DB_NAME="CHANGE_ME"
+DB_FILE="$DB_NAME-$NOW.sql"
 
 timestamp() {
-        date +"%Y/%m/%d %H:%M"
+        date +'%Y/%m/%d %I:%M %p'
 }
 
 log_message () {
-        echo -e "$1\n" >> "$LOG_FILE" 2>&1
+        echo -e "$1" >> "$LOG_FILE" 2>&1
 }
 
 execute() {
-        $1 >> "${LOG_FILE}" 2>&1
+    eval "$1" 2>> ${LOG_FILE}
+    #print_result $? "${2:-$1}"
+    if [ "$?" -eq 0 ]; then
+      log_message "OK: ${2:-$1}"
+      return 0
+    else
+      log_message "ERROR: $2 Failed! Exiting..."
+      exit 1
+    fi
 }
 
+# Check if tools exist
+if [ ! -x "$(command -v aws)" ]; then
+   log_message "ERROR - AWS CLI not installed. Exiting..."
+   exit 1
+fi
 
+if [ ! -x "$(command -v /opt/bitnami/mysql/bin/mysqldump)" ]; then
+   log_message "ERROR - MySQLDump not found. Exiting..."
+   exit 1
+fi
 # Test for backup directory
 if [[ ! -d  "${BACKUP_DIR}" ]]; then
-        mkdir "${BACKUP_DIR}"
-        log_message "Creating backup directory"
+        execute "mkdir ${BACKUP_DIR}" "Create ${BACKUP_DIR}"
 fi
 
-# Create the archive and the MySQL dump
 log_message ""
-log_message "######## BACKUP START: $(timestamp) ########"
-
-log_message "- Backing up $(basename ${WWW_DIR}) to ${BACKUP_DIR}"
-execute "tar --exclude=var/cache --exclude=var/session -cf ${BACKUP_DIR}/${FILE} -C $(dirname ${WWW_DIR}) $(basename ${WWW_DIR})" #Site files
-
-log_message "Backing up MySQL database: $DB_NAME"
-mysqldump --user="${DB_USER}" --password="${DB_PASS}" --add-drop-table "${DB_NAME}" > /tmp/"${DB_FILE}" 2>> "${LOG_FILE}"
-#mysqldump -u$DB_USER -p$DB_PASS -$DB_NAME > $BACKUP_DIR/$DB_FILE
-
+log_message "BACKUP STARTED: $(timestamp)"
+execute "tar --exclude=var/cache --exclude=var/session -cf ${BACKUP_DIR}/${FILE} -C $(dirname ${WWW_DIR}) $(basename ${WWW_DIR})" \
+"Create TAR archive from ${WWW_DIR} in ${BACKUP_DIR}"
+# MYSQLDUMP
+execute '/opt/bitnami/mysql/bin/mysqldump --user="${DB_USER}" --password="${DB_PASS}" --add-drop-table "${DB_NAME}" > /tmp/"${DB_FILE}"' "Mysqldump to ${DB_FILE}"
+log_message "OK: DB \"${DB_FILE}\" size: $(du -h /tmp/${DB_FILE} | awk '{ print $1 }')"
 # Append the dump to the archive, remove the dump and compress the whole archive.
-log_message "Creating archive"
-execute "tar --append --file=${BACKUP_DIR}/${FILE} -C /tmp/ ${DB_FILE}"
-
-log_message "Removing temprorary database dump file"
-execute "rm -v /tmp/${DB_FILE}"
-
-log_message "Zipping up the archive"
-execute "gzip -9 ${BACKUP_DIR}/${FILE}"
-
-# Upload to S3 Bucket
-if [ -x "$(command -v aws)" ]; then
-        log_message "Uploading ${FILE}.gz to Amazon S3 "
-        execute "aws s3 cp ${BACKUP_DIR}/${FILE}.gz ${S3_BUCKET}"
-        log_message "Removing local backup file"
-        execute "rm -v ${BACKUP_DIR}/${FILE}.gz"
-else
-        log_message "AWS CLI is not installed. Backup file not uploaded to S3!"
-fi
-
+execute "tar --append --file=${BACKUP_DIR}/${FILE} -C /tmp/ ${DB_FILE}" "Append ${DB_FILE} to ${FILE}.gz"
+# log_message "Removing temprorary database dump file"
+execute "rm /tmp/${DB_FILE}" "Remove /tmp/${DB_FILE}"
+# log_message "Zipping up the archive"
+execute "gzip -f -9 ${BACKUP_DIR}/${FILE}" "Gunzip to ${FILE}.gz"
+GZ_TOTAL_FILESIZE=$(du -h ${BACKUP_DIR}/"${FILE}".gz | awk '{ print $1 }')
+# execute "aws s3 cp ${BACKUP_DIR}/${FILE}.gz ${S3_BUCKET}" "Upload ${FILE}.gz to ${S3_BUCKET}"
+execute "aws s3 cp ${LOG_FILE} ${S3_BUCKET}" "Upload ${LOG_FILE} to ${S3_BUCKET}"
+execute "rm ${BACKUP_DIR}/${FILE}.gz" "Remove local ${BACKUP_DIR}/${FILE}.gz"
 # Finish
-log_message "######## BACKUP COMPLETE ########"
+log_message "SUCCESS: $(timestamp), FILE: ${FILE}.gz, SIZE: $GZ_TOTAL_FILESIZE"
