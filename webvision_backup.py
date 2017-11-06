3253cb1739347d25889bf43a4fe53987dbda67e5
Tadeusz Swider <tadeusz.swider@motorolasolutions.com>
2017-11-06 17:29:31 +0100

    Add args, cleanup, fix issues

--- webvision_backup.py
+++ webvision_backup.py
@@ -9,15 +9,12 @@ import subprocess
 import datetime
 import getopt
 
-# Variable definitions
+# Global Variables
+WEB_DIR = '/home/bitnami/apps/magento/htdocs'
+EXCLUDE_DIRS = ['session', 'cache'] # Directories to exclude
+S3_BUCKET = 's3://webvision-backups'
 
-# SITE_NAME = 'magento1-52-73-17-216'
-
-# CREDENTIALS_FILE = '/home/bitnami/credentials.txt'
-
-
-
-
+# Function Definitions
 def read_creds(cred_file):
     '''Read credentials'''
     try:
@@ -33,18 +30,38 @@ def read_creds(cred_file):
             elif value1 == 'DB_NAME':
                 global DB_NAME
                 DB_NAME = value2
-            elif value1 == 'SITE_NAME':
-                global SITE_NAME
-                SITE_NAME = value2
             else:
                 logging.error('Coorrect values not supplied!')
                 sys.exit('Correct values not supplied! Exiting...')
     except FileNotFoundError:
         logging.error('File %s not found!', cred_file)
 
+def usage():
+    '''Print Usage'''
+    print('backup.py --site-name=magento1 --credentials-file=credentials.txt')
+    print('backup.py -s magento1 -c credentials.txt')
+
+def my_opts():
+    '''Get command line options'''
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hs:c:", ["help", "site-name=", "credentials-file="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    cmd_opts = dict()
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit()
+        elif opt in ("-s", "--site-name"):
+            cmd_opts['site'] = arg
+        elif opt in ("-c", "--credentials-file"):
+            cmd_opts['creds'] = arg
+    return cmd_opts
+
 def zipdir(path, ziph, exclude):
     '''Zip up the directory excluding some subdirs'''
-    logging.info('Create zip archive from %s and %s', WEB_DIR, DB_FILE)
+    logging.info('Create zip archive from %s excluding %s', path, exclude)
     # ziph is zipfile handle
     for dirname, subdirs, files in os.walk(path):
         subdirs[:] = [d for d in subdirs if d not in exclude]
@@ -54,7 +71,7 @@ def zipdir(path, ziph, exclude):
 def database_bkp(dbuser, dbpass, dbname, dbfile):
     '''Backup Database'''
     logging.info('MySQLDump DB: %s', dbname)
-    logging.info('Credentials info: DB: %s, User: %s, Password: %s', DB_NAME, DB_USER, DB_PASS)
+    logging.info('Credentials info: DB: %s, User: %s, Password: %s', dbname, dbuser, dbpass)
     try:
         with open(dbfile, 'wb', 0) as f, open('backup.log', 'a') as e:
             subprocess.call(['mysqldump', '-u', dbuser, '-p' + dbpass, '--add-drop-database', \
@@ -74,63 +91,41 @@ def s3_upload(s3file, s3bucket):
         logging.error('AWS CLI encounter critical error: ' + str(err) + '\nExiting...')
         sys.exit('AWS CLI encounter critical error! Exiting...')
 
-def rm_local(rm_file):
+def cleanup(*args):
     '''Remove local file'''
-    logging.info('Removing local backup fil %s', rm_file)
     try:
-        os.remove(rm_file)
-    except FileNotFoundError as fnf:
-        logging.error('The %s file was not found. Exiting...', rm_file)
-        sys.exit(fnf)
+        for arg in args:
+            logging.info('Removing local file: %s', arg)
+            os.remove(arg)
+    except FileNotFoundError as err:
+        logging.error('The %s file was not found. Exiting...', args)
+        sys.exit(err)
 
-def usage():
-    '''Print Usage'''
-    print('backup.py --site-name=magento1 --credentials-file=credentials.txt')
-    print('backup.py -s magento1 -c credentials.txt')
-
-def main(argv):
+def main():
     '''Main script'''
-    logging.info('### Backup started: ' + datetime.datetime.now().strftime("%Y-%m-%d %I:%M%p") \
+    opts = my_opts()
+    credentials_file = opts['creds']
+    site_name = opts['site']
+    logging.basicConfig(filename=site_name + '.backup.log', format='%(levelname)s:%(message)s', level=logging.INFO)
+    logging.info('\n### Backup started: ' + datetime.datetime.now().strftime("%Y-%m-%d %I:%M%p") \
     + ' ###')
-    # Parse variables from command line
-    try:
-        opts, args = getopt.getopt(argv, "hs:c:", ["help", "site-name=", "credentials-file="])
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            usage()
-            sys.exit()
-        elif opt in ("-s", "--site-name"):
-            SITE_NAME = arg
-        elif opt in ("-c", "--credentials-file"):
-            CREDENTIALS_FILE = arg
 
-    WEB_DIR = '/home/bitnami/apps/magento/htdocs'
-    EXCLUDE_DIRS = ['session', 'cache'] # Directories to exclude
-    DB_FILE = SITE_NAME +'.sql'
-    ZIP_FILE = SITE_NAME + '_' + datetime.datetime.now().strftime("%y%m%d_%H%M") + '.zip'
-    S3_BUCKET = 's3://webvision-backups'
-    logging.basicConfig(filename='backup.log', format='%(levelname)s:%(message)s', level=logging.INFO)
-    # Read Credentials
-    read_creds(CREDENTIALS_FILE)
+    db_file = site_name +'.sql'
+    zip_file = site_name + '_' + datetime.datetime.now().strftime("%Y%m%d_%H%M") + '.zip'
+    read_creds(credentials_file)
 
     # Backup Database
-    database_bkp(DB_USER, DB_PASS, DB_NAME, DB_FILE)
+    database_bkp(DB_USER, DB_PASS, DB_NAME, db_file)
 
     # Archive web directory
-    zipf = zipfile.ZipFile(ZIP_FILE, 'w', zipfile.ZIP_DEFLATED)
+    zipf = zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED)
     zipdir(WEB_DIR, zipf, EXCLUDE_DIRS)
-    zipf.write(DB_FILE)
+    zipf.write(db_file)
     zipf.close()
 
-    # Upload to S3
-    s3_upload(ZIP_FILE, S3_BUCKET)
-
-    # Remove local file
-    rm_local('test.file')
+    s3_upload(zip_file, S3_BUCKET) # Upload to S3
+    cleanup(zip_file, db_file) # Remove local files
     logging.info('Success ' + datetime.datetime.now().strftime("%Y-%m-%d %I:%M%p"))
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
